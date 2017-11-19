65f92a6a79e8c7a2f83f3c99d7cf1047d9dbfde7
egpavelka <pav@egpavelka.com>
2017-11-19 14:01:12 -0500

    substitute files for symlinks, and terminal emulator color schemes

--- rofi/rofi.sh
+++ rofi/rofi.sh
@@ -1,1 +1,4 @@
-/usr/local/bin/rofi.sh
+#!/bin/bash
+
+/usr/bin/rofi -show combi -location 0 -lines 9 -bw 0 -font "Inconsolata 13" -separator-style solid -fullscreen -padding 300 -hide-scrollbar -terminal terminator -kb-cancel 'Escape,Super+space'
+
