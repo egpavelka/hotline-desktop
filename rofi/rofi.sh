9f9edd9873ac80fc73aa0b90a6749c83200d40dc
egpavelka <pav@egpavelka.com>
2017-11-26 04:08:04 -0500

    major fixes to GTK themes, added updater, added configs for emacs, MC

--- rofi/rofi.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/usr/bin/rofi -show combi -location 0 -lines 9 -bw 0 -font "Inconsolata 13" -separator-style solid -fullscreen -padding 300 -hide-scrollbar -terminal terminator -kb-cancel 'Escape,Super+space'
-
